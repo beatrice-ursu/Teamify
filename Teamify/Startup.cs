2fd2857f5907a1bb93f0072d8c1d61c4f3e7214e
Beatrice Ramona <beatrisuu@gmail.com>
2017-05-07 00:30:02 +0300

    Add project files.

---
+++ Teamify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Teamify.Startup))]
+namespace Teamify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
