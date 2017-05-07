9c1e6ebe4a24042eb75f392b8be7de4e7c329180
Beatrice Ramona <beatrisuu@gmail.com>
2017-05-07 12:45:30 +0300

    Style changes

---
+++ Teamify/Helpers/HtmlHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Teamify.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string IsCurrent(this HtmlHelper html, string controllers = "", string actions = "", string cssClass = "active")
+        {
+            var viewContext = html.ViewContext;
+            var isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+            if (isChildAction)
+                viewContext = html.ViewContext.ParentActionViewContext;
+
+            var routeValues = viewContext.RouteData.Values;
+            var currentAction = routeValues["action"].ToString();
+            var currentController = routeValues["controller"].ToString();
+
+            if (string.IsNullOrEmpty(actions))
+                actions = currentAction;
+
+            if (string.IsNullOrEmpty(controllers))
+                controllers = currentController;
+
+            var acceptedActions = actions.Trim().Split(',').Distinct().ToArray();
+            var acceptedControllers = controllers.Trim().Split(',').Distinct().ToArray();
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+                cssClass : string.Empty;
+        }
+    }
+}
