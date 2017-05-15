13689859e93736d753a39820484878eb60ce622b
Beatrice Ramona <beatrisuu@gmail.com>
2017-05-15 22:22:43 +0300

    Login and Register

--- Teamify/DL/Entities/UserProfile.cs
+++ Teamify/DL/Entities/UserProfile.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -16,6 +17,7 @@ namespace Teamify.DL.Entities
         [DefaultValue(5)]
         public float Rating { get; set; }
 
+        public virtual ICollection<Sport> FavoriteSports { get; set; }
         public virtual User User { get; set; }
     }
 }
